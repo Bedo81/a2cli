1ecf7e2de1633c2ace458eff3804df88dd966c68
Martin Vacho <Martin.Vacho@allianz.at>
2017-02-27 21:24:40 +0100

    search box

--- src/app/trips/search-box.component.spec.ts
+++ src/app/trips/search-box.component.spec.ts
@@ -3,7 +3,102 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
+import { Observable } from 'rxjs/Rx';
+
 import { SearchBoxComponent } from './search-box.component';
+
+import { DataService } from '../shared/services/data.service';
+
+let trips = [
+  {
+    "id": "1",
+    "country": "BnnVietnam",
+    "description": "fdbdf dsvdfb",
+    "dates": [
+      {
+        "start": "2017-02-21T13:28:06.419Z",
+        "end": "2017-03-08T13:28:06.419Z"
+      },
+      {
+        "start": "2017-02-23T13:28:06.419Z",
+        "end": "2017-03-15T13:28:06.419Z"
+      },
+      {
+        "start": "2017-03-03T13:28:06.419Z",
+        "end": "2017-03-15T13:28:06.419Z"
+      }
+    ]
+  },
+  {
+    "id": "2",
+    "country": "BnmAustria",
+    "description": "RRRR Ausrt",
+    "dates": [
+      {
+        "start": "2017-02-15T13:28:06.419Z",
+        "end": "2017-03-15T13:28:06.419Z"
+      },
+      {
+        "start": "2017-02-16T13:28:06.419Z",
+        "end": "2017-03-08T13:28:06.419Z"
+      },
+      {
+        "start": "2017-03-01T13:28:06.419Z",
+        "end": "2017-03-15T13:28:06.419Z"
+      }
+    ]
+  },
+  {
+    "id": "3",
+    "country": "BnxxxSlovakia",
+    "description": "bgfnfgn",
+    "dates": [
+      {
+        "start": "2017-02-21T13:28:06.419Z",
+        "end": "2017-03-08T13:28:06.419Z"
+      },
+      {
+        "start": "2017-02-23T13:28:06.419Z",
+        "end": "2017-03-15T13:28:06.419Z"
+      },
+      {
+        "start": "2017-03-03T13:28:06.419Z",
+        "end": "2017-03-15T13:28:06.419Z"
+      }
+    ]
+  },
+  {
+    "id": "4",
+    "country": "BThailand",
+    "description": "jvusvst vscvsvool",
+    "dates": [
+      {
+        "start": "2017-02-21T13:28:06.419Z",
+        "end": "2017-03-08T13:28:06.419Z"
+      },
+      {
+        "start": "2017-02-23T13:28:06.419Z",
+        "end": "2017-03-15T13:28:06.419Z"
+      },
+      {
+        "start": "2017-03-03T13:28:06.419Z",
+        "end": "2017-03-15T13:28:06.419Z"
+      }
+    ]
+  },
+  {
+    "id": "5",
+    "country": "Xxxx",
+    "description": "DDDDD"
+  }
+]
+
+class DataServiceMock {
+
+  getTrips() {
+    return Observable.of(...trips);
+  }
+}
 
 describe('SearchBoxComponent', () => {
   let component: SearchBoxComponent;
@@ -11,7 +106,8 @@ describe('SearchBoxComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SearchBoxComponent ]
+      declarations: [ SearchBoxComponent ],
+      providers: [ {provide: DataService, useClass: DataServiceMock} ]
     })
     .compileComponents();
   }));
--- src/app/trips/search-box.component.ts
+++ src/app/trips/search-box.component.ts
@@ -1,7 +1,10 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output } from '@angular/core';
+
+import {Observable} from 'rxjs/Rx';
+
+import {DataService} from '../shared/services/data.service';
 
 @Component({
-  outputs: ["loading", "results"],
   selector: 'app-search-box',
   template: `
     <p>
@@ -11,13 +14,37 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styles: []
 })
 export class SearchBoxComponent implements OnInit {
-  loading: EventEmitter<boolean> = new EventEmitter<boolean>();
-  results: EventEmitter<any[]> = new EventEmitter<any>();
+  @Output() loading: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() results: EventEmitter<any[]> = new EventEmitter<any>();
 
 
-  constructor() { }
+  constructor(public dataService: DataService,
+              private el: ElementRef) { }
 
   ngOnInit() {
+    // convert the `keyup` event into an observable stream
+    Observable.fromEvent(this.el.nativeElement, "keyup")
+              .map((e: any) => e.target.value) // extract the value of the input
+              .filter((text: string) => text.length > 1) // filter out if empty
+              .debounceTime(250)                             // only once every 250ms
+              .do(() => this.loading.next(true))         // enable loading
+              // search, discarding old events if new input comes in
+              .map((query: string) => this.dataService.getTrips(query))
+              .switch()
+              // act on the return of the search
+              .subscribe(
+                (results: any[]) => { // on sucesss
+                  this.loading.next(false);
+                  this.results.next(results);
+                },
+                (err: any) => { // on error
+                  console.log(err);
+                  this.loading.next(false);
+                },
+                () => { // on completion
+                  this.loading.next(false);
+                }
+              );
   }
 
 }
